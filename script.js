a6641b6f4cbb755834dd1b0a43f89027ed4d7219
João Vittor Venturini <92241015+JoaoVittorVenturini@users.noreply.github.com>
2026-05-05 12:46:40 -0300

    Update gifts

--- script.js
+++ script.js
@@ -6,243 +6,243 @@ const gifts = [
   {
     "id": 1,
     "name": "Deus te iluminou e você resolveu dar uma boa ajuda",
-    "price": 1500.01,
+    "price": 5000.01,
     "image": "./gifts/iluminado.jpg",
-    "max": 5,
+    "max": 999,
     "bought": 0,
     "link": ""
   },
   {
     "id": 2,
     "name": "Cota do Colchão King Emma",
-    "price": 700.02,
+    "price": 1125.02,
     "image": "./gifts/colchao-emma.jpg",
-    "max": 5,
+    "max": 3,
     "bought": 0,
     "link": "https://www.mercadolivre.com.br/emma-original-classic-brancocinza-king-203-cm-193-cm-25-cm/p/MLB15810522?product_trigger_id=MLB17351734&picker=true&quantity=1"
   },
   {
     "id": 3,
+    "name": "Ar Condicionado",
+    "price": 3100.03,
+    "image": "./gifts/ar-condicionado.jpg",
+    "max": 1,
+    "bought": 0,
+    "link": "https://www.mercadolivre.com.br/ar-condicionado-lg-dual-inverter-ai-voice-12000-btus-qf-220v/up/MLBU3810727091"
+  },
+  {
+    "id": 4,
+    "name": "Echo Show 5",
+    "price": 700.04,
+    "image": "./gifts/echo-show-5.jpg",
+    "max": 1,
+    "bought": 0,
+    "link": "https://www.amazon.com.br/Echo-Show-5-3%C2%AAgera%C3%A7%C3%A3o-Cor-Preta/dp/B09B2TSNNN"
+  },
+  {
+    "id": 5,
+    "name": "Amigos para sempre",
+    "price": 750.05,
+    "image": "./gifts/amigos-para-sempre.png",
+    "max": 2,
+    "bought": 0,
+    "link": ""
+  },
+  {
+    "id": 6,
+    "name": "Aparelho de Jantar 30 peças",
+    "price": 550.06,
+    "image": "./gifts/aparelho-jantar.jpg",
+    "max": 1,
+    "bought": 0,
+    "link": "https://www.mercadolivre.com.br/oxford-ryo-maresia-branco-liso-30/p/MLB34115624?product_trigger_id=MLB64883205&picker=true&quantity=1"
+  },
+  {
+    "id": 7,
+    "name": "Cota Lua de Mel em Bariloche",
+    "price": 550.07,
+    "image": "./gifts/bariloche.jpg",
+    "max": 10,
+    "bought": 0,
+    "link": ""
+  },
+  {
+    "id": 8,
+    "name": "Cota do Forno e Fritadeira Oster",
+    "price": 350.08,
+    "image": "./gifts/forno-eletrico.jpg",
+    "max": 3,
+    "bought": 0,
+    "link": "https://www.mercadolivre.com.br/forno-e-fritadeira-sem-oleo-oster-42l-french-door-2-em-1-cor-prateado-tssttvfdmaf/p/MLB22477586"
+  },
+  {
+    "id": 9,
     "name": "Cota da TV 55' QLED",
-    "price": 500.03,
+    "price": 500.09,
     "image": "./gifts/tv.jpg",
     "max": 6,
     "bought": 0,
     "link": "https://www.mercadolivre.com.br/samsung-vision-ai-tv-55-qled-ultra-4k-qef1-2025/p/MLB48957045"
   },
   {
-    "id": 4,
-    "name": "Cota do Ar Condicionado",
-    "price": 300.04,
-    "image": "./gifts/ar-condicionado.jpg",
-    "max": 10,
-    "bought": 0,
-    "link": "https://www.mercadolivre.com.br/ar-condicionado-lg-dual-inverter-ai-voice-12000-btus-qf-220v/up/MLBU3810727091"
-  },
-  {
-    "id": 5,
-    "name": "Vaquinha para ajudar os noivos quebrados",
-    "price": 200.05,
-    "image": "./gifts/porquinho.jpeg",
-    "max": 10,
-    "bought": 0,
-    "link": ""
-  },
-  {
-    "id": 6,
-    "name": "Cota Lua de Mel em Bariloche",
-    "price": 550.06,
-    "image": "./gifts/bariloche.jpg",
-    "max": 15,
-    "bought": 0,
-    "link": ""
-  },
-  {
-    "id": 7,
-    "name": "Cota dos Móveis Planejados",
-    "price": 200.07,
-    "image": "./gifts/planejados.jpg",
-    "max": 15,
-    "bought": 0,
-    "link": ""
-  },
-  {
-    "id": 8,
-    "name": "Jantar romântico dos noivos",
-    "price": 180.08,
-    "image": "./gifts/jantar.jpg",
-    "max": 6,
-    "bought": 0,
-    "link": ""
-  },
-  {
-    "id": 9,
-    "name": "Mercado do mês dos noivos",
-    "price": 150.09,
-    "image": "./gifts/mercado.jpg",
-    "max": 10,
-    "bought": 0,
-    "link": ""
-  },
-  {
     "id": 10,
-    "name": "Pizza da sexta-feira sagrada",
-    "price": 150.10,
-    "image": "./gifts/pizza.jpg",
-    "max": 4,
+    "name": "Jogo de Panelas",
+    "price": 425.10,
+    "image": "./gifts/jogo-panelas.jpg",
+    "max": 1,
     "bought": 0,
-    "link": ""
+    "link": "https://www.mercadolivre.com.br/jogo-de-panelas-antiaderente-com-15-pecas-em-aluminio-resistente-cabo-removivel-para-facil-armazenamentocomp-preto-ativel-com-fogo-a-gas-e-eletricoideal-para-uso-diariocozinha-pratica-preto/p/MLB63670413"
   },
   {
     "id": 11,
-    "name": "Café da manhã especial dos noivos",
-    "price": 100.11,
-    "image": "./gifts/cafe.jpg",
-    "max": 4,
+    "name": "Vaquinha para ajudar os noivos quebrados",
+    "price": 400.11,
+    "image": "./gifts/porquinho.jpeg",
+    "max": 8,
     "bought": 0,
     "link": ""
   },
   {
     "id": 12,
-    "name": "Boletos dos noivos",
-    "price": 100.12,
-    "image": "./gifts/boletos.jpg",
-    "max": 15,
+    "name": "Primeira compra no mercado dos noivos",
+    "price": 325.12,
+    "image": "./gifts/mercado.jpg",
+    "max": 1,
     "bought": 0,
     "link": ""
   },
   {
-    "id": 14,
-    "name": "Colcha King",
-    "price": 250.14,
-    "image": "./gifts/colcha.jpg",
-    "max": 3,
-    "bought": 0,
-    "link": ""
-  },
-  {
-    "id": 15,
-    "name": "Jogo de Panelas",
-    "price": 275.15,
-    "image": "./gifts/jogo-panelas.jpg",
-    "max": 2,
-    "bought": 0,
-    "link": "https://www.mercadolivre.com.br/jogo-de-panelas-antiaderente-com-15-pecas-em-aluminio-resistente-cabo-removivel-para-facil-armazenamentocomp-preto-ativel-com-fogo-a-gas-e-eletricoideal-para-uso-diariocozinha-pratica-preto/p/MLB63670413"
-  },
-  {
-    "id": 16,
-    "name": "Decoração da casa",
-    "price": 200.16,
-    "image": "./gifts/decoracoes.jpg",
-    "max": 5,
-    "bought": 0,
-    "link": ""
-  },
-  {
-    "id": 17,
-    "name": "Curso de gastronomia para a noiva",
-    "price": 200.17,
-    "image": "./gifts/gastronomia.jpg",
-    "max": 2,
-    "bought": 0,
-    "link": ""
-  },
-  {
-    "id": 18,
+    "id": 13,
     "name": "Academia dos noivos",
-    "price": 300.18,
+    "price": 300.13,
     "image": "./gifts/academia.jpg",
     "max": 4,
     "bought": 0,
     "link": ""
   },
   {
-    "id": 19,
-    "name": "Cueca sensual para o noivo usar na noite de núpcias",
-    "price": 120.19,
-    "image": "./gifts/cueca.jpg",
-    "max": 2,
+    "id": 14,
+    "name": "Cota dos Móveis Planejados",
+    "price": 300.14,
+    "image": "./gifts/planejados.jpg",
+    "max": 8,
     "bought": 0,
     "link": ""
   },
   {
-    "id": 20,
-    "name": "Rolo de macarrão para momentos necessários",
-    "price": 100.20,
-    "image": "./gifts/rolo.jpg",
-    "max": 1,
-    "bought": 0,
-    "link": ""
-  },
-  {
-    "id": 21,
-    "name": "Capacete contra o rolo",
-    "price": 285.21,
+    "id": 15,
+    "name": "Capacete contra rolo de macarrão",
+    "price": 285.15,
     "image": "./gifts/capacete.jpg",
     "max": 1,
     "bought": 0,
     "link": ""
   },
   {
-    "id": 22,
+    "id": 16,
+    "name": "Decoração da casa",
+    "price": 275.16,
+    "image": "./gifts/decoracoes.jpg",
+    "max": 6,
+    "bought": 0,
+    "link": ""
+  },
+  {
+    "id": 17,
+    "name": "Colcha King size",
+    "price": 250.17,
+    "image": "./gifts/colcha.jpg",
+    "max": 2,
+    "bought": 0,
+    "link": ""
+  },
+  {
+    "id": 18,
     "name": "Makita pra cortar a unha do noivo",
-    "price": 225.22,
+    "price": 225.18,
     "image": "./gifts/makita.jpg",
     "max": 1,
     "bought": 0,
     "link": ""
   },
   {
-    "id": 23,
-    "name": "Máscara de gás para futuras fraldas",
-    "price": 100.23,
-    "image": "./gifts/mascara.jpg",
+    "id": 19,
+    "name": "Primeira conta de luz do casal (ajuda psicológica inclusa)",
+    "price": 200.19,
+    "image": "./gifts/boletos.jpg",
     "max": 2,
     "bought": 0,
     "link": ""
   },
   {
+    "id": 20,
+    "name": "Curso de gastronomia para a noiva",
+    "price": 180.20,
+    "image": "./gifts/gastronomia.jpg",
+    "max": 2,
+    "bought": 0,
+    "link": ""
+  },
+  {
+    "id": 21,
+    "name": "Jantar romântico dos noivos",
+    "price": 200.21,
+    "image": "./gifts/jantar.jpg",
+    "max": 8,
+    "bought": 0,
+    "link": ""
+  },
+  {
+    "id": 22,
+    "name": "Café da manhã especial dos noivos",
+    "price": 165.22,
+    "image": "./gifts/cafe.jpg",
+    "max": 4,
+    "bought": 0,
+    "link": ""
+  },
+  {
+    "id": 23,
+    "name": "Pizza da sexta-feira sagrada",
+    "price": 150.23,
+    "image": "./gifts/pizza.jpg",
+    "max": 4,
+    "bought": 0,
+    "link": ""
+  },
+  {
     "id": 24,
-    "name": "Amigos para sempre",
-    "price": 200.24,
-    "image": "./gifts/amigos-para-sempre.png",
-    "max": 5,
+    "name": "Cueca sensual para o noivo usar na noite de núpcias",
+    "price": 149.24,
+    "image": "./gifts/cueca.jpg",
+    "max": 4,
     "bought": 0,
     "link": ""
   },
   {
     "id": 25,
-    "name": "Cota do Forno e Fritadeira Oster",
-    "price": 200.25,
-    "image": "./gifts/forno-eletrico.jpg",
-    "max": 5,
+    "name": "Máscara de gás para futuras fraldas",
+    "price": 125.25,
+    "image": "./gifts/mascara.jpg",
+    "max": 1,
     "bought": 0,
-    "link": "https://www.mercadolivre.com.br/forno-e-fritadeira-sem-oleo-oster-42l-french-door-2-em-1-cor-prateado-tssttvfdmaf/p/MLB22477586"
+    "link": ""
   },
   {
     "id": 26,
-    "name": "Cota do Echo Show 5",
-    "price": 140.26,
-    "image": "./gifts/echo-show-5.jpg",
-    "max": 5,
+    "name": "Rolo de macarrão para momentos necessários",
+    "price": 100.26,
+    "image": "./gifts/rolo.jpg",
+    "max": 1,
     "bought": 0,
-    "link": "https://www.amazon.com.br/Echo-Show-5-3%C2%AAgera%C3%A7%C3%A3o-Cor-Preta/dp/B09B2TSNNN"
+    "link": ""
   },
   {
     "id": 27,
-    "name": "Cota do Aparelho de Jantar 30 peças",
-    "price": 150.27,
-    "image": "./gifts/aparelho-jantar.jpg",
-    "max": 4,
-    "bought": 0,
-    "link": "https://www.mercadolivre.com.br/oxford-ryo-maresia-branco-liso-30/p/MLB34115624?product_trigger_id=MLB64883205&picker=true&quantity=1"
-  },
-  {
-    "id": 28,
     "name": "O que Deus tocar no seu coração",
     "price": 0,
     "image": "./gifts/oque-tocar-no-coracao.jpg",
-    "max": 25,
+    "max": 10,
     "bought": 0,
     "link": ""
   }
